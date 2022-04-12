2678315218c31ab5f40f70427e08530daf6f4cd4
Juaanpa <santos.majua21@salesianos.zaragoza.edu>
2022-04-12 18:52:09 +0200

    first look

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3E545E',
+        secondary: '#ba0',
+      },
+    },
+  },
+  plugins: [],
+}
