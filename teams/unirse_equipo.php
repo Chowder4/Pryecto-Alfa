f925c4607ce35eb1443ed3336fef13d763fb6e82
noe <moros.grnoe21@zaragoza.salesianos.edu>
2022-05-21 20:47:19 +0200

    Subo archivos editados

--- teams/unirse_equipo.php
+++ teams/unirse_equipo.php
@@ -5,6 +5,10 @@ $registro=mysqli_query($conexion,$sql) or die ("Error en la consulta $sql");
 $num=mysqli_num_rows($registro);
 if($num==0){
     echo "No se encontro equipo con dicho nombre";
+    $sql2="UPDATE usuario as u, equipos as e
+        SET u.id_team=e.id_equipo";
+    $sql3="UPDATE Usuario as u, equipos as e
+    set u.nombre_equipo=e.nombre_equipo";
 } else
     header("location:./paginaprincipal_equipo.html");
 ?>
